2c929790187a541d3433ae6a7c572108ac74ae0f
KhanJabeer <khan.jabeer01@gmail.com>
2020-12-03 00:47:20 +0530

    jjd

---
+++ machine-test/src/Components/UsermanageModal/UserdeleteModal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './UsermanageModal.css'
+
+
+
+const UserdeleteModal = () => {
+
+    
+
+  return(
+    <div className="">
+     
+     
+    
+         <div className="del_content">Are You Sure Want to Delete This User?</div>
+
+         <div className="btn_wraps">
+        <button className="cancel_btn">No</button>    
+        <button className="addedit_btn">Yes</button>
+    </div>     
+    
+
+    
+
+    
+
+    </div>
+  )
+  }
+
+
+export default UserdeleteModal;
