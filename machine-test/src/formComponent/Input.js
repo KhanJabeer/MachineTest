d26788d8faefa9317e9f7d1dd46c5cbdab494247
iLRNUAdmin <Nyc10025!>
2020-12-05 17:38:18 +0530

    input component added

---
+++ machine-test/src/formComponent/Input.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const Input = ({label,required,value,placeholder,changeData,errmsg,type,className}) => {
+    console.log("errr",errmsg)
+    return(
+        <div>
+            <div className="field_label">
+            <label>{label && label}</label>
+                 {required && <span>*</span>}
+            </div>
+
+            <div>
+            <input className={className && className} type={type && type} value={value && value} placeholder={placeholder && placeholder} onChange={(e) => changeData && changeData(e.target.value) } />
+            <div>
+                {errmsg && errmsg}
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Input;
